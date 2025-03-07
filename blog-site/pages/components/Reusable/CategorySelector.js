cf72f4bc0711b63698385c5db1ef0c37e9148bba
dhanesh1232 <dhaneshreddy980@gmail.com>
2025-03-07 23:50:47 +0530

    Fixed undefined categories issue

--- blog-site/pages/components/Reusable/CategorySelector.js
+++ blog-site/pages/components/Reusable/CategorySelector.js
@@ -6,20 +6,24 @@ const CategorySelector = ({ page, services }) => {
 
   return (
     <div className="w-full flex justify-center flex-wrap gap-3 mt-4">
-      {services.map((service) => (
-        <button
-          key={service.slug}
-          onClick={() => router.push(`${page}/${service.slug}`)}
-          className={`px-4 py-2 rounded-lg transition ease-in-out duration-150 text-base md:text-lg font-medium 
+      {services?.length > 0 ? (
+        services.map((service) => (
+          <button
+            key={service.slug}
+            onClick={() => router.push(`${page}/${service.slug}`)}
+            className={`px-4 py-2 rounded-lg transition ease-in-out duration-150 text-base md:text-lg font-medium 
             ${
               category === service.slug
                 ? "bg-blue-500 text-white dark:bg-blue-400 dark:text-gray-900"
                 : "bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-gray-300 hover:bg-green-600 hover:text-white"
             }`}
-        >
-          {service.label}
-        </button>
-      ))}
+          >
+            {service.label}
+          </button>
+        ))
+      ) : (
+        <p>Loading categories...</p>
+      )}
     </div>
   );
 };
