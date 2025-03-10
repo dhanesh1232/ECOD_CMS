0363066057b00d635c26eede55ee08e929b6ad6c
dhanesh1232 <dhaneshreddy980@gmail.com>
2025-03-10 23:06:29 +0530

    buttons changed

--- blog-site/pages/components/Home/hero-section.js
+++ blog-site/pages/components/Home/hero-section.js
@@ -97,7 +97,7 @@ const HeroSection = () => {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, delay: 0.3 }}
-        className="flex flex-col sm:flex-row gap-4 z-10"
+        className="flex gap-2 sm:gap-4 z-10"
       >
         <Buttons
           first_label={"Learn More"}
